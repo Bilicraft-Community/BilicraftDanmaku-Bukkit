18e7c85d327f3067a1ce85c9d1a2db32b4b03531
lxy <1304043543@qq.com>
2021-08-08 14:30:14 +0800

    实现基本功能

---
+++ src/main/java/com/bilicraft/bilicraftdanmaku/BilicraftDanmaku.java
@@ -0,0 +1,32 @@
+package com.bilicraft.bilicraftdanmaku;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+public final class BilicraftDanmaku extends JavaPlugin {
+
+    public static final String pluginName = "BilicraftDanmaku";
+    public static Logger logger;
+    public static BilicraftDanmaku Instance;
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        BilicraftDanmaku.logger = getLogger();
+        BilicraftDanmaku.Instance = this;
+        DanmakuListener listener = new DanmakuListener();
+        Bukkit.getPluginManager().registerEvents(listener,this);
+        Bukkit.getMessenger().registerIncomingPluginChannel(this, "bilicraftclientui:bilicraftdanmaku" , listener);
+        Bukkit.getMessenger().registerOutgoingPluginChannel(this,"bilicraftclientui:bilicraftdanmaku" );
+        logger.info(pluginName + "is loaded, Hello, world!");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+
+
+}
---
+++ src/main/java/com/bilicraft/bilicraftdanmaku/ServerConfigs.java
@@ -0,0 +1,64 @@
+package com.bilicraft.bilicraftdanmaku;
+
+import com.google.common.collect.Sets;
+
+import java.io.File;
+import java.util.Set;
+
+public class ServerConfigs {
+    // private static Configuration config;
+
+    public static String allowedMode = "0;1;2;3";
+    public static int minLifespan = 10;
+    public static int maxLifespan = 100;
+    public static int commentInterval = 100;
+    public static boolean whitelistMode = true;
+    public static boolean appendUsername = true;
+    public static boolean allowLANServer = false;
+    public static boolean showSenderNameOnComment = true;
+
+    private static Set<Integer> sModes = Sets.newHashSet();
+    private static int hModes = 0;
+
+    public static boolean isModeAllowed(int mode){
+        if (hModes != allowedMode.hashCode()){
+            sModes.clear();
+            for (String s : allowedMode.split(";"))
+            {
+                try
+                {
+                    sModes.add(Integer.parseInt(s));
+                }
+                catch (NumberFormatException ignored)
+                {
+                }
+            }
+            hModes = allowedMode.hashCode();
+        }
+        return sModes.contains(mode);
+    }
+
+    public static void load(File configFile){
+//        config = new Configuration(configFile);
+//        sync();
+    }
+
+    public static void reload(){
+//        config.load();
+//        sync();
+    }
+
+//    protected static void sync(){
+//        allowedMode = config.get(Configuration.CATEGORY_GENERAL, "allowedMode", "0;1;2").getString();
+//        minLifespan = config.get(Configuration.CATEGORY_GENERAL, "minLifespan", 40).getInt(40);
+//        maxLifespan = config.get(Configuration.CATEGORY_GENERAL, "maxLifespan", 400).getInt(400);
+//        commentInterval = config.get(Configuration.CATEGORY_GENERAL, "commentInterval", 100).getInt(100);
+//        whitelistMode = config.get(Configuration.CATEGORY_GENERAL, "whitelistMode", true).getBoolean(true);
+//        appendUsername = config.get(Configuration.CATEGORY_GENERAL, "appendUsername", true).getBoolean(true);
+//        allowLANServer = config.get(Configuration.CATEGORY_GENERAL, "allowLANServer", false).getBoolean(false);
+//
+//        if (config.hasChanged())
+//            config.save();
+//    }
+
+}
